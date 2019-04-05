13a676a7d97449ecd6417ed386863cd9784f7163
OttawaSTEM <ottawastem@gmail>
2019-04-05 12:14:03 -0400

    remove postgresql

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,4 @@
 # source ./scripts/start.sh
-pg_ctl -D /usr/local/var/postgres start
 . /Volumes/Data/Workspace/Django/DjangoBase/venv/bin/activate
 cd /Volumes/Data/Workspace/Django/DjangoBase/src
 ./manage.py runserver 0.0.0.0:8000
