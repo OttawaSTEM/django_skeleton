eec5e1427d52e0e8f7dde36fb84763818364ca39
OttawaSTEM <ottawastem@gmail.com>
2025-06-29 10:57:10 -0400

    cleanup email debug

--- django_skeleton/settings/email.py
+++ django_skeleton/settings/email.py
@@ -7,9 +7,5 @@ if 'smtp' in EMAIL_CONFIG['EMAIL_BACKEND']:
     EMAIL_PORT = EMAIL_CONFIG['EMAIL_PORT']
     EMAIL_HOST_USER = EMAIL_CONFIG['EMAIL_HOST_USER']
     EMAIL_HOST_PASSWORD = EMAIL_CONFIG['EMAIL_HOST_PASSWORD']
-else:
-    # For developing, email sent to console
-    # Commen local.env "EMAIL_URL" to use console Email backend
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 WEBMASTERS = eval(env("WEBMASTERS"))
